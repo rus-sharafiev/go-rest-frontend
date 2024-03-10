d3ff0ee31dfb3312f804a35c8e76f2b12c874b8b
Rus Sharafiev <srr@live.ru>
2024-03-10 18:40:00 +0300

    Change auth info fields

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,15 +2,15 @@ import FetchApi from "@russh/fetch-api"
 
 // --------------------------------------------------------------------------------
 
-export const BASE_URL: string = 'http://10.10.10.100:5555'
+export const BASE_URL: string = 'http://10.10.10.100:5555/api'
 
 export enum ApiRoutes {
-    SIGNUP = '/api/auth/signup',
-    VERIFY_EMAIL = '/api/auth/signup/verify',
-    SIGNIN = '/api/auth/login',
-    LOGOUT = '/api/auth/logout',
-    GET_AUTH_USER = '/api/auth/user',
-    TOKEN_REFRESH = '/api/auth/refresh',
+    SIGNUP = '/auth/signup',
+    VERIFY_EMAIL = '/auth/signup/verify',
+    SIGNIN = '/auth/login',
+    LOGOUT = '/auth/logout',
+    GET_AUTH_USER = '/auth/user',
+    TOKEN_REFRESH = '/auth/refresh',
 }
 
 export const api = new FetchApi(BASE_URL, ApiRoutes.TOKEN_REFRESH, { convertToFormData: true })
