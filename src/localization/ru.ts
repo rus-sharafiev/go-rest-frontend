e85cb7d1ad9c168012538941fc05e992ede046c6
Rus Sharafiev <srr@live.ru>
2024-03-18 23:03:32 +0300

    Add groups to localization file and some minor layout fixes

--- src/localization/ru.ts
+++ src/localization/ru.ts
@@ -1,10 +1,20 @@
 export const ru: Record<string, string> = {
+    // Buttons    
+    'Save': 'Сохранить',
+    'Submit': 'Подтвердить',
+    'Back': 'Назад',
+    'Log Out': 'Выйти',
+
+    // Navigation
+    'Home': 'Главная',
+    'Account settings': 'Настройки аккаунта',
+    'Photo converter': 'Фото конвертер',
+
+    // Auth
     'Sign In': 'Вход',
     'Please sign in to access site content': 'Авторизутесь, чтобы получить доступ к сайту',
     'Sign Up': 'Регистрация',
     'An email with a confirmation code will be sent to the email address you provided during registration': 'На адрес электронной почты, указанный при регистрации, будет отправлено письмо с кодом подтверждения',
-    'Submit': 'Подтвердить',
-    'Back': 'Назад',
     'Cancel registration': 'Отменить регистрацию',
     'Email': 'Email',
     'Password': 'Пароль',
@@ -23,12 +33,9 @@ export const ru: Record<string, string> = {
     'provided email': 'Указанную ранее',
     'Must be exactly 6 characters': 'Код должен состоять из 6 цифр',
     'Code is valid until': 'Код действителен',
-    'Log Out': 'Выйти',
-    'Settings': 'Настройки',
-    'Account settings': 'Настройки аккаунта',
-    'Photo converter': 'Фото конвертер',
+
+    // User account settings
     'First name': 'Имя',
     'Last name': 'Фамилия',
     'Phone number': 'Номер телефона',
-    'Save': 'Сохранить',
 }
